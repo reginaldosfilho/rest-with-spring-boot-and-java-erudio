2e1dc0506b685c5849d272c9f713242a071183a6
Reginaldo Freitas <reginaldo.freitas@uece.br>
2022-09-24 09:27:58 -0300

    adicionado seção 11 do curso

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Pupulate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(6, 'Africa', 'Nelson', 'Male', 'Mandela'),
+	(7, 'Croacia', 'Nikola', 'Male', 'Tesla');
